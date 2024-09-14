4eb825cc4d85755cfe57a2e9d3f3600df61fd87e
Shaurya-Ps-Bisht <shauryawbisht@gmail.com>
2024-09-14 16:14:43 +0530

    first commit

---
+++ src/main/java/com/shaurya/javApp/JavAppApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.javApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaurya/javApp/JavAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaurya.javApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
